2a37178790def66640d913747ab49423d20a5f42
yosseftag272006-jpg <yosseftag272006@gmail.com>
2026-07-19 00:26:05 +0300

    Add files via upload

---
+++ CP.c
@@ -0,0 +1,21 @@
+#include<fcntl.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+int cp_main(int argc, char *argv[]) {
+char buff[100];
+int source_fd;
+int destination_fd;
+int checks;
+int checkd;
+source_fd =open(argv[1],O_RDONLY);
+if (source_fd<0)
+return -1;
+destination_fd=open(argv[2],O_RDWR | O_CREAT | O_TRUNC,0644);
+if(destination_fd<0)
+return -2;
+while((checks=read(source_fd,buff,100))>0){
+    write(destination_fd,buff,checks);
+}
+    return 0;
+}
