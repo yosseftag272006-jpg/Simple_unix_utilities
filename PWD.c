809b3ec4dff9213ae4f0a4a5e47dadfc2b2bf0e9
yosseftag272006-jpg <yosseftag272006@gmail.com>
2026-07-20 13:21:33 -0400

    Delete PWD.c

--- PWD.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-int pwd_main() {
-    char buff[2000];
-    if((getcwd(buff,sizeof(buff)))!=NULL);
-    write(1,buff,strlen(buff));
-    return 0;
-}
