e9eeb65f17d7964360c0177368d5e307bd992389
yosseftag272006-jpg <yosseftag272006@gmail.com>
2026-07-28 22:55:59 +0300

    picoshell.c

--- picoshell.c
+++ picoshell.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
   while (1)
     {
@@ -19,33 +18,49 @@ main (int argc, char **argv)
 	{
 	  continue;
 	}
-      if (!strcmp (buff, "exit"))
+      char *argv2[128];
+      int no = 0;
+      char my_string[128][4096];
+      int index = 0;
+      for (int i = 0; i <= strlen (buff); i++)
+	{
+	  if (buff[i] != ' ' && buff[i] != '\0')
+	    {
+	      my_string[no][index] = buff[i];
+	      index++;
+	    }
+	  else if (index > 0)
+	    {
+	      my_string[no][index] = '\0';
+	      argv2[no] = my_string[no];
+	      index = 0;
+	      no++;
+	    }
+	}
+      argv2[no] = NULL;
+      if (no == 0)
+	{
+	  continue;
+	}
+      if (!strcmp (argv2[0], "exit"))
 	{
 	  printf ("Good Bye \n");
 	  return 0;
 	}
-      else if (!strncmp (buff, "echo ", 5))
+      else if (!strcmp (argv2[0], "pwd"))
 	{
-	  printf ("%s\n", buff + 5);
-	}
-      else if (!strcmp (buff, "echo"))
-	{
-	  printf ("\n");
-	}
-      else if (!strcmp (buff, "pwd"))
-	{
-	  char pwd_buff[20000];
-	  if (getcwd (pwd_buff, 20000) != NULL)
+	  char pwd_buff[4096];
+	  if (getcwd (pwd_buff, 4096) != NULL)
 	    {
 	      printf ("%s\n", pwd_buff);
 	    }
 	}
-      else if (!strncmp (buff, "cd ", 3))
+      else if (!strcmp (argv2[0], "cd"))
 	{
-	  if (chdir (buff + 3) != 0)
+	  if (chdir (argv2[1]) != 0)
 	    {
 	      printf ("picoshell: cd: %s: no such file or directory \n",
-		      buff + 3);
+		      argv2[1]);
 	    }
 	}
       else
@@ -57,26 +72,6 @@ main (int argc, char **argv)
 	    }
 	  else if (pid == 0)
 	    {
-	      char *argv2[10];
-	      int no = 0;
-	      char my_string[10][100];
-	      int index = 0;
-	      for (int i = 0; i <= strlen (buff); i++)
-		{
-		  if (buff[i] != ' ' && buff[i] != '\0')
-		    {
-		      my_string[no][index] = buff[i];
-		      index++;
-		    }
-		  else if (index > 0)
-		    {
-		      my_string[no][index] = '\0';
-		      argv2[no] = my_string[no];
-		      index = 0;
-		      no++;
-		    }
-		}
-	      argv2[no] = NULL;
 	      if (execvp (argv2[0], argv2) == -1)
 		{
 		  printf ("invalid command\n");
